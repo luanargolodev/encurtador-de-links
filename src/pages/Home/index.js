06577de8b1531f22f6a661cee65f2fbb51f810f8
Luan Argolo <ipsluan@icloud.com>
2022-02-06 21:50:15 -0300

    Finalizar estilização da página

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,23 @@
+import { FiLink } from "react-icons/fi";
+import "./home.css";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Página Home!</h1>
+    <div className="container-home">
+      <div className="logo">
+        <img src="/logo.png" alt="Logo" />
+        <h1>Encurtador de Links</h1>
+        <span>Encurte os seus links de forma rápida e eficaz! ✂️</span>
+      </div>
+
+      <div className="area-input">
+        <div>
+          <FiLink size={24} color="#ffffff" />
+          <input type="text" placeholder="Cole seu link aqui" />
+        </div>
+
+        <button>Encurtar link</button>
+      </div>
     </div>
   );
 }
